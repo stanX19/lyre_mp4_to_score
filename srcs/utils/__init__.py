496aba188c5b6d1b94f07d04e6153e2e7a8b602f
StanX19 <tanshanchien@gmail.com>
2024-03-08 16:59:49 +0800

    relative_import

--- srcs/utils/__init__.py
+++ srcs/utils/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from .common import *
 from .converter import *
 from .unique_name import *
